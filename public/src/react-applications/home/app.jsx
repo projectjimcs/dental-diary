a3af4ea33b5ebfd517afba4d8f88cad2f7f777ba
Jim Lin <projectjimcs@gmail.com>
2020-03-03 22:55:18 -0800

    Initial setup of home react component

---
+++ public/src/react-applications/home/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = document.querySelector('#app');
+// const props = element.dataset;
+
+ReactDOM.render(
+  <div>Hello testing</div>, element
+);
+
+// const render = (Component) => {
+//   ReactDOM.render(
+//     <AppContainer>
+//       <Component {...props}/>
+//     </AppContainer>,
+//     element
+//   );
+// };
+
+// render(JobApplication);
