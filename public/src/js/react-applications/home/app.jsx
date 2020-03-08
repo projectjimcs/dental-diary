e0a4b77ed4ae9745f556b6b5d968b7516ca11b7b
Jim Lin <projectjimcs@gmail.com>
2020-03-07 18:32:39 -0800

    Removed unnecessary code

--- public/src/js/react-applications/home/app.jsx
+++ public/src/js/react-applications/home/app.jsx
@@ -2,19 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const element = document.querySelector('#app');
-// const props = element.dataset;
 
 ReactDOM.render(
   <div>Hello testing</div>, element
 );
-
-// const render = (Component) => {
-//   ReactDOM.render(
-//     <AppContainer>
-//       <Component {...props}/>
-//     </AppContainer>,
-//     element
-//   );
-// };
-
-// render(JobApplication);
